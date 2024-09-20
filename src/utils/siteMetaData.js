e48fb131cf85e8415b43558b2dc9df2226051f59
JardsonVenancio <126803792+jardson123442@users.noreply.github.com>
2024-09-20 10:28:54 -0300

    Fix

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -1,7 +1,7 @@
 // please update required information
 const siteMetadata = {
   title: 'RotaTech | Home',
-  author: 'CodeBucks | Iuri Torres',
+  author: 'CodeBucks | Team',
   headerTitle: 'RotaTech',
   description: 'Plataforma de cursos introdutórios de programação da RotaTech.',
   language: 'pt-br',
@@ -9,12 +9,11 @@ const siteMetadata = {
   siteUrl: 'https://tech-courses-plataform.vercel.app/', // your website URL
   siteLogo: '/logo-svg.svg',
   socialBanner: '/social-banner.png', // add social banner in the public folder
-  email: 'iuri.t1000@gmail.com',
-  github: 'https://github.com/iuritorres',
+  github: 'https://github.com',
   twitter: 'https://twitter.com',
   facebook: 'https://facebook.com',
-  youtube: 'https://www.youtube.com/channel/UCoTWXGqXvuL-u6vAad4ezBQ',
-  linkedin: 'https://www.linkedin.com/in/iuri-torres/',
+  youtube: 'https://www.youtube.com',
+  linkedin: 'https://www.linkedin.com',
   dribbble: 'https://www.dribbble.com',
   locale: 'pt-BR',
 }
